e1fae752ffd355150474792fa2a2cba68fc7e758
oladayo ahmod <oladayoahmod112@gmail.com>
2023-07-31 23:49:41 -0700

    bug fix

--- scripts/weth.js
+++ scripts/weth.js
@@ -1,17 +1,15 @@
-const {ethers} = require('hardhat')
+const {getNamedAccounts, ethers} = require('hardhat')
 
 const AMOUNT = ethers.utils.parseEther('0.02')
 async function getWeth() {
-    const [deployer] = await ethers.getSigners(); // get the deployer
-    const contract = await ethers.getContractAt('Iweth', // get the contract
-    '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
-    deployer
+    const [deployer] = await ethers.getSigners() // get the deployer
+    const contract = await ethers.getContractAt('IWeth','0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',deployer
     )
 
     const tx =await contract.deposit({value : AMOUNT})
-    tx.wait(1)
-    const wethBalance = await contract.balanceOf(deployer)
-    console.log(wethBalance);
+    await tx.wait(1)
+    const wethBalance = await contract.balanceOf(deployer.address)
+    console.log(wethBalance.toString());
 }
 
 module.exports = {

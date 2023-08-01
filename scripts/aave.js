861a07a9e1c8319ec9e92e5687506c05774fd81b
oladayo ahmod <oladayoahmod112@gmail.com>
2023-08-01 05:06:14 -0700

    retrieving lending pool address

--- scripts/aave.js
+++ scripts/aave.js
@@ -1,12 +1,23 @@
 const { ethers } = require('hardhat')
-const {getWeth,lendingPool} = require('../scripts/weth')
+const {getWeth} = require('../scripts/weth')
 async function main(){
     await getWeth()
     const [deployer] = await ethers.getSigners()
-    const lending_Pool = await lendingPool(deployer.address)
-    console.log(`lending pool contract address is ${lending_Pool}`);
+    const lending_Pool = await lendingPool(deployer)
+    console.log(`lending pool contract address is ${lending_Pool.address}`);
 }
 
+async function lendingPool(account){
+    const contract = await ethers.getContractAt(
+    'ILendingPoolAddressesProvider',
+    '0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5',
+    account)
+
+    const lendingPoolAddress = await contract.getLendingPool()
+    const lending_Pool = await ethers.getContractAt('ILendingPool',lendingPoolAddress,account)
+    return lending_Pool
+
+}
 main()
 .then(()=> process.exit(0))
 .catch((error)=>{
--- scripts/weth.js
+++ scripts/weth.js
@@ -1,8 +1,8 @@
 const {getNamedAccounts, ethers} = require('hardhat')
 
 const AMOUNT = ethers.utils.parseEther('0.02')
-const [deployer] = await ethers.getSigners() // get the deployer
 async function getWeth() {
+    const [deployer] = await ethers.getSigners() // get the deployer
     const contract = await ethers.getContractAt('IWeth','0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',deployer
     )
 
@@ -12,19 +12,6 @@ async function getWeth() {
     console.log(wethBalance.toString());
 }
 
-async function lendingPool(account){
-    const contract = await ethers.getContractAt(
-    'ILendingPoolAddressesProvider',
-    '0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5',
-    account)
-
-    const lendingPoolAddress = await contract.getLendingPool()
-    const lending_Pool = await ethers.getContractAt('ILendingPool',lendingPoolAddress,account)
-    return lending_Pool
-
-
-}
 module.exports = {
     getWeth,
-    lendingPool
 }

f4d8c8fc029365ebd74074a1adda83743a50dc77
oladayo ahmod <oladayoahmod112@gmail.com>
2023-08-01 05:13:29 -0700

    add erc20 interface

--- scripts/aave.js
+++ scripts/aave.js
@@ -3,11 +3,11 @@ const {getWeth} = require('../scripts/weth')
 async function main(){
     await getWeth()
     const [deployer] = await ethers.getSigners()
-    const lending_Pool = await lendingPool(deployer)
+    const lending_Pool = await getLendingPool(deployer)
     console.log(`lending pool contract address is ${lending_Pool.address}`);
 }
 
-async function lendingPool(account){
+async function getLendingPool(account){
     const contract = await ethers.getContractAt(
     'ILendingPoolAddressesProvider',
     '0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5',
@@ -17,6 +17,10 @@ async function lendingPool(account){
     const lending_Pool = await ethers.getContractAt('ILendingPool',lendingPoolAddress,account)
     return lending_Pool
 
+}
+
+async function approveErc20(){
+    
 }
 main()
 .then(()=> process.exit(0))
